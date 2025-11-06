e03dd1788c7bf077649e1ee90d4709daa9cb460b
Syaifullah Abdul Fattah <105910658+syaif112233@users.noreply.github.com>
2025-11-07 00:53:19 +0700

    Update supabaseClient.js

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,1 +1,8 @@
+// js/supabaseClient.js
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
+
+const SUPABASE_URL = 'https://<YOUR_PROJECT>.supabase.co'
+const SUPABASE_ANON_KEY = '<YOUR_ANON_KEY>'
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
 
