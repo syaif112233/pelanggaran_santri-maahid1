04e0d5cdc9209479661d6a3ab7174d091aa8d029
Syaifullah Abdul Fattah <105910658+syaif112233@users.noreply.github.com>
2025-11-07 02:28:02 +0700

    Update supabaseClient.js

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -4,11 +4,4 @@ import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
 const SUPABASE_URL = 'https://qjcnqeyrzryuclwpmala.supabase.co'
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFqY25xZXlyenJ5dWNsd3BtYWxhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI0MDIwNjgsImV4cCI6MjA3Nzk3ODA2OH0.WwYGa4gKjoJ8n4fuqpQifgpLPJxHlZuUbtdhiF0OGs8'
 
-// Guard kecil
-if (!SUPABASE_URL || !SUPABASE_ANON_KEY) {
-  throw new Error('Supabase URL/ANON key belum terisi.')
-}
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
-  auth: { persistSession: false, autoRefreshToken: false },
-})
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
