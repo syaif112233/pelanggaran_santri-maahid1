607e0532c5444a0107574bcf722248060beb414f
Syaifullah Abdul Fattah <105910658+syaif112233@users.noreply.github.com>
2025-11-08 11:50:08 +0700

    Update supabaseClient.js

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,14 +1,9 @@
-// js/supabaseClient.js
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
 
 const SUPABASE_URL = 'https://qjcnqeyrzryuclwpmala.supabase.co'
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFqY25xZXlyenJ5dWNsd3BtYWxhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI0MDIwNjgsImV4cCI6MjA3Nzk3ODA2OH0.WwYGa4gKjoJ8n4fuqpQifgpLPJxHlZuUbtdhiF0OGs8'
 
-const client = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-
-// expose ke global agar bisa dipakai file lain
-window.supabase = client;
-
-// beri sinyal ke listener lain kalau sudah siap
-window.dispatchEvent(new Event('supabase-ready'));
+const client = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
+window.supabase = client
+window.dispatchEvent(new Event('supabase-ready'))
 
