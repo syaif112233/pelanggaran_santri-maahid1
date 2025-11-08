55cf930205fd040f712e42b92ddf8bb7a2878440
Syaifullah Abdul Fattah <105910658+syaif112233@users.noreply.github.com>
2025-11-08 11:54:07 +0700

    Update supabaseClient.js

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -7,3 +7,14 @@ const client = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
 window.supabase = client
 window.dispatchEvent(new Event('supabase-ready'))
 
+//import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
+
+//const SUPABASE_URL = 'https://qjcnqeyrzryuclwpmala.supabase.co'
+//const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFqY25xZXlyenJ5dWNsd3BtYWxhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI0MDIwNjgsImV4cCI6MjA3Nzk3ODA2OH0.WwYGa4gKjoJ8n4fuqpQifgpLPJxHlZuUbtdhiF0OGs8'
+
+//window.supabase = createClient(supabaseUrl, supabaseKey);
+
+// beri tahu app-logic kalau sudah siap
+//window.dispatchEvent(new Event('supabase-ready'));
+
+
